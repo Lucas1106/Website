59ad003d747841938942ef5e0a258b6a1ed035ec
Lucas1106 <lucas.cesar1998@hotmail.com>
2025-02-03 00:06:20 -0500

    Add files via upload

---
+++ CleanerManager/backend/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+const { PrismaClient } = require('@prisma/client');
+
+dotenv.config();
+const app = express();
+const prisma = new PrismaClient();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Cleaner Manager API is running...');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
